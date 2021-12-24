1906bf55dd4e9a2eae15e6fccca9de33b1602a2b
Maxim Eustigneev <34115509+euxxx@users.noreply.github.com>
2021-12-24 22:32:09 +0300

    re

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,1 @@
-mvn clean && mvn package -f "/home/magnus/Sources/Studying/magmacrypt/pom.xml" && cd ./TEST_DATA/OUTPUT/ && rm -rf ENCRYPTED KEY RESTORED.ZIP SINGLE_BINARY_FILE.jpeg
+mvn clean && mvn package -f "./pom.xml" && cd ./TEST_DATA/OUTPUT/ && rm -rf ENCRYPTED KEY RESTORED.ZIP SINGLE_BINARY_FILE.jpeg
